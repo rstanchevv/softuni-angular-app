0bb4b1c8f27cd38247c277db5828b0ebb836fdb6
Radoslav Stanchev <rstanchev93@abv.bg>
2024-03-20 09:30:20 +0200

    apiKey hidden

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { AboutComponent } from './about/about.component';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { environment } from 'src/environments/environment.development';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,7 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
     TechModule,
     HomeModule,
     AppRoutingModule,
-    provideFirebaseApp(() => initializeApp({"projectId":"techkings-angular-demo-project","appId":"1:101881152932:web:1981f8cc0e53741daeed31","storageBucket":"techkings-angular-demo-project.appspot.com","apiKey":"AIzaSyD2C3ogSa0HyPkA4ysZZgrIe6IR62YPiDs","authDomain":"techkings-angular-demo-project.firebaseapp.com","messagingSenderId":"101881152932","measurementId":"G-BLZG6JMF0V"})),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
   ],
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'techkings-angular-demo-project',
+    appId: '1:101881152932:web:1981f8cc0e53741daeed31',
+    storageBucket: 'techkings-angular-demo-project.appspot.com',
+    apiKey: 'AIzaSyD2C3ogSa0HyPkA4ysZZgrIe6IR62YPiDs',
+    authDomain: 'techkings-angular-demo-project.firebaseapp.com',
+    messagingSenderId: '101881152932',
+    measurementId: 'G-BLZG6JMF0V',
+  },
+};
